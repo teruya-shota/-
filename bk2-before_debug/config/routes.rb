aab12fb645cebf0b8e4e7111d10fab8c698340ad
照屋翔大 <shota@mac.local>
2020-09-07 13:04:00 +0900

    finish debug2

---
+++ bk2-before_debug/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root 'home#top'
+
+  get 'home/about' => 'home#about'
+  resources :books
+  resources :users,only: [:show,:index,:edit,:update]
+end
